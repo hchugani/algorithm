9220650c4226972090bb8e9bf3f05c615612668a
hchugani <chugani.hitesh@gmail.com>
2021-12-08 13:48:48 -0600

    HashSet: longest consecution subsequnce

---
+++ src/Array/longest_consec_subsequence.py
@@ -0,0 +1,52 @@
+from typing import List
+
+"""
+128. Longest Consecutive Sequence
+Medium
+
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+
+"""
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        O(N) both
+        """
+        explored = set(nums)
+
+        longest = 0
+        for num in nums:
+            if num-1 not in explored:
+                cur = num
+                cur_streak = 1
+
+                while cur+1 in explored:
+                    cur_streak+=1
+                    cur+=1
+
+                longest = max(cur_streak, longest)
+        return longest
+
+s = Solution()
+inputs = [
+    [100,4,200,1,3,2],
+    [0],
+    [],
+    [0,1,2,4,8,5,6,7,9,3,55,88,77,99,999999999]
+]
+
+for nums in inputs:
+    print(s.longestConsecutive(nums))
