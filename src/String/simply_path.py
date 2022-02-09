4231d6e2106b446d74f5d5a1d65b06e5845992c6
hchugani <chugani.hitesh@gmail.com>
2022-02-08 23:49:27 -0500

    Strings: simply path

---
+++ src/String/simply_path.py
@@ -0,0 +1,19 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+
+        stack = []
+
+        for portion in path.split("/"):
+
+            if portion == "." or not portion:
+                continue
+            elif portion == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(portion)
+
+        return "/" + "/".join(stack)
+
+s = Solution()
+print(s.simplifyPath("/a/b/c/.././././//d"))
